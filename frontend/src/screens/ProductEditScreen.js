2ba0556c444298fb0f4a25888ec228c5923eb0fd
Parth Puri <parthpuri2000@gmail.com>
2021-03-05 23:50:36 +0530

    Adding image upload functionality to ProductEditScreen

--- frontend/src/screens/ProductEditScreen.js
+++ frontend/src/screens/ProductEditScreen.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 import {Link} from 'react-router-dom'
 import { Form, Button} from 'react-bootstrap'
 import {useDispatch, useSelector} from 'react-redux'
@@ -19,6 +20,7 @@ const ProductEditScreen = ({match, history}) => {
     const [category, setCategory] = useState('')
     const [countInStock, setCountInStock] = useState(0)
     const [description, setDescription] = useState('')
+    const [uploading, setUploading] = useState(false)
     
     const dispatch = useDispatch()
     
@@ -49,6 +51,28 @@ const ProductEditScreen = ({match, history}) => {
 
 
     }, [dispatch, history, productId, product, successUpdate])
+
+    const uploadFileHandler = async(e) => {
+        const file = e.target.files[0]
+        const formData = new FormData()
+        formData.append('image', file)
+        setUploading(true)
+
+        try{
+            const config = {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            }
+            const {data} = await axios.post('/api/upload', formData, config)
+            setImage(data)
+            setUploading(false)
+            
+        }catch(error){
+            console.log(error)
+            setUploading(false)
+        }
+    } 
 
     const submitHandler = (e)=> {
         e.preventDefault()
@@ -89,6 +113,8 @@ const ProductEditScreen = ({match, history}) => {
                 <Form.Group controlId="image">
                     <Form.Label>Image</Form.Label>
                     <Form.Control type="text" placeholder="Enter Image URL" value={image} onChange={(e)=> setImage(e.target.value)}></Form.Control>
+                    <Form.File id="image-file" label="Choose File" custom onChange={uploadFileHandler}></Form.File>
+                    {uploading && <Loader/>}
                 </Form.Group>
 
                 <Form.Group controlId="brand">
